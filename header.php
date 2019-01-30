f1c4fa7365dd3ec737847f22e1614b94b68c3c10
fangline <44398578+fangline@users.noreply.github.com>
2019-01-30 16:56:24 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,235 @@
+<div id="header">
+	<nav class="navbar navbar-expand-lg navbar-dark  shadow-sm border-bottom border-warning  fixed-top" style="background-color: rgba(17,9,9,.4);border-color:#d6ba7f; border-width: 2px !important; ">
+		<div class="container-fluid ">
+			<a class="navbar-brand text-center  " style="color:#f34d47 ;font-size:30px;margin-bottom:0px;line-height:35px " href="index.php">
+			<!--	<img src="img/logo2-01.png" class="align-top hvr-grow" height="35"><b>&nbsp;大漢傢具</b>-->
+<!-- logo  svg -->
+			<svg class="hvr-grow" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="250px" height="40px" viewBox="0 0 250.899 40.297" enable-background="new 0 0 250.899 40.297" xml:space="preserve">
+						<g id="圖層_2">
+						</g>
+						
+						<g id="圖層_1">
+							<g>
+								<path fill="#E45348" d="M133.227,35.335c-5.959-4.624-9.842-9.621-11.477-14.891l-0.511,1.428    c-2.418,6.153-6.914,11.015-13.385,14.584l-2.146-2.448c7.833-3.604,12.567-9.791,14.1-18.46h-12.976v-2.992h13.282l0.102-1.7    V5.894h3.134v4.964l-0.068,1.7h13.249v2.992h-12.875c0.102,1.326,0.375,2.516,0.715,3.569c1.771,5.644,6.232,10.777,13.351,15.4    l-2.623,2.21L133.227,35.335z"/>
+								<path fill="#E45348" d="M139.76,35.131c2.554-3.536,4.188-7.377,4.836-11.559l2.725,0.476c-1.021,5.235-2.656,9.349-4.836,12.273    L139.76,35.131z M145.039,20.546c-2.009-1.734-3.746-2.992-5.177-3.74l1.669-2.414c1.566,0.884,3.338,2.142,5.278,3.842    L145.039,20.546z M145.481,12.795c-2.009-1.836-3.712-3.196-5.143-4.045l1.771-2.312c1.601,0.952,3.371,2.312,5.244,4.147    L145.481,12.795z M169.253,21.362c0,0.238-0.136,0.374-0.374,0.374h-8.48v1.428h9.264v2.481h-9.264l-0.103,1.428h10.422v2.651    h-9.468c2.146,2.584,5.551,4.114,10.148,4.42l-0.852,2.788c-5.108-0.374-8.957-2.312-11.477-5.711    c-1.771,3.366-5.381,4.929-11.478,5.813l-1.022-2.856c5.449-0.34,8.89-1.87,10.286-4.454h-8.787v-2.651h9.468l0.103-1.428h-8.651    v-2.481h8.651v-1.428h-8.515c-0.238,0-0.34-0.136-0.34-0.374v-5.78c0-0.204,0.102-0.306,0.34-0.306h8.515v-1.496h-4.462    c-0.749,0-1.123-0.408-1.123-1.156V9.531h-4.769V6.914h4.769V5.418h2.758v1.496h8.412V5.418h2.759v1.496h5.007v2.618h-5.007v3.094    c0,0.748-0.374,1.156-1.157,1.156h-4.428v1.496h8.48c0.238,0,0.374,0.102,0.374,0.306V21.362z M157.64,17.623h-6.097    c-0.102,0-0.136,0.034-0.102,0.102v1.53l0.102,0.136h6.097V17.623z M162.817,11.401c0.272,0,0.408-0.102,0.408-0.408V9.531h-8.412    v1.462c0,0.306,0.137,0.408,0.409,0.408H162.817z M166.495,17.623h-6.096v1.768h6.096l0.137-0.136v-1.53L166.495,17.623z"/>
+								<path fill="#E45348" d="M190.635,21.498c-5.007,3.875-9.673,6.357-13.964,7.411l-1.362-2.618c5.108-1.36,9.877-3.773,14.168-7.241    l-1.158-1.394c-4.121,2.788-8.071,4.59-11.716,5.338l-1.192-2.55c4.121-0.918,7.936-2.584,11.479-4.896h-7.425v-2.754h20.639    v2.754h-8.89l-0.612,0.476c1.021,1.19,1.907,2.516,2.521,4.012c1.805,4.352,4.154,9.111,12.635,14.177l-2.281,2.142    c-3.645-2.414-6.676-5.235-9.025-8.363c-0.068,3.876-1.295,8.874-5.619,8.874c-0.716,0-1.567-0.068-4.871-1.156l0.137-3.06    c2.043,0.68,3.542,1.02,4.495,1.02c0.307,0,0.545-0.034,0.749-0.102c1.567-0.612,2.385-4.114,2.385-6.595    c-5.688,4.25-11.001,7.003-15.905,8.057l-1.328-2.652c6.335-1.632,12.022-4.521,16.927-8.601L190.635,21.498z M188.456,7.899    V5.248h2.86v2.652h11.921l2.52,1.258c-0.238,1.972-0.783,4.284-1.736,7.003l-2.623-0.952c0.512-1.598,1.09-3.536,1.09-4.216    c0-0.238-0.136-0.34-0.375-0.34h-24.146c-0.17,0-0.272,0.102-0.272,0.272v4.759h-2.759V8.443c0-0.374,0.171-0.544,0.545-0.544    H188.456z M194.926,21.43c2.35-1.904,4.087-3.604,5.245-5.032l2.009,1.972c-1.294,1.598-3.099,3.332-5.381,5.167L194.926,21.43z"/>
+								<path fill="#E45348" d="M212.527,26.938V7.594c0-0.612,0.307-0.952,0.954-0.952h21.013c0.682,0,0.987,0.34,0.987,0.952v19.344    h3.883v2.652H208.44v-2.652H212.527z M208.678,34.077c3.27-0.442,6.402-1.7,9.332-3.739l1.635,2.379    c-3.679,2.414-7.016,3.808-9.911,4.148L208.678,34.077z M215.421,11.673h17.165V9.667c0-0.272-0.102-0.408-0.408-0.408h-16.382    c-0.272,0-0.375,0.136-0.375,0.408V11.673z M215.421,16.977h17.165v-2.686h-17.165V16.977z M215.421,21.906h17.165v-2.278h-17.165    V21.906z M215.421,26.938h17.165v-2.414h-17.165V26.938z M238.104,37c-3.984-1.836-7.221-3.06-9.809-3.706l0.988-2.754    c2.895,0.68,6.164,1.938,9.91,3.74L238.104,37z"/>
+							</g>
+							<rect fill="#E45348" width="98.771" height="40.297"/>
+							<circle fill="#0099B2" cx="21.537" cy="5.309" r="2.191"/>
+							<rect x="45.073" y="11.502" fill="#A91C21" width="8.999" height="3.597"/>
+							<rect x="45.525" y="28.514" fill="#A91C21" width="8.998" height="3.597"/>
+							<g>
+								<path fill="#FFFFFF" d="M31.752,15.201c2.382,0,4.127,0.467,5.237,1.401c1.109,0.934,1.665,2.104,1.665,3.512v1.949v8.891v1.178    c0,0.487-0.088,1.021-0.264,1.604c-0.176,0.582-0.521,1.117-1.035,1.604c-0.514,0.487-1.218,0.893-2.111,1.218    c-0.894,0.325-2.057,0.487-3.492,0.487c-1.435,0-2.598-0.162-3.492-0.487c-0.893-0.325-1.597-0.731-2.111-1.218    c-0.515-0.487-0.86-1.021-1.036-1.604s-0.264-1.117-0.264-1.604v-1.908c0-1.028,0.264-1.963,0.792-2.802s1.279-1.434,2.253-1.787    c0.487-0.163,1.042-0.351,1.665-0.568c0.622-0.216,1.225-0.426,1.807-0.629s1.089-0.386,1.523-0.548    c0.432-0.163,0.703-0.257,0.812-0.285c0.107-0.054,0.25-0.128,0.426-0.223c0.175-0.095,0.346-0.237,0.508-0.426    c0.162-0.189,0.305-0.406,0.426-0.649c0.123-0.244,0.183-0.528,0.183-0.853v-0.771c0-0.271-0.054-0.548-0.162-0.833    c-0.108-0.284-0.298-0.548-0.568-0.791c-0.271-0.244-0.63-0.44-1.076-0.589c-0.447-0.148-1.009-0.223-1.685-0.223    c-1.354,0-2.267,0.278-2.74,0.832c-0.474,0.556-0.71,1.226-0.71,2.011h-3.451v-0.975c0-0.676,0.128-1.312,0.386-1.908    c0.257-0.595,0.67-1.117,1.238-1.563s1.285-0.798,2.151-1.056C29.492,15.33,30.534,15.201,31.752,15.201z M35.244,25.96    c-0.054,0.271-0.19,0.474-0.406,0.609c-0.217,0.135-0.42,0.23-0.609,0.284c-0.406,0.136-0.846,0.291-1.319,0.467    c-0.474,0.176-0.914,0.332-1.319,0.467c-0.487,0.162-0.948,0.325-1.38,0.487c-0.325,0.136-0.637,0.325-0.934,0.569    c-0.244,0.217-0.467,0.494-0.67,0.832c-0.203,0.339-0.304,0.765-0.304,1.278c0,1.029,0.244,1.794,0.73,2.294    c0.487,0.501,1.394,0.751,2.72,0.751c1.354,0,2.274-0.264,2.761-0.792c0.486-0.528,0.73-1.279,0.73-2.254V25.96z"/>
+								<path fill="#FFFFFF" d="M57.249,7.285V36.72h-3.41V23.605h-7.754V36.72h-3.451V7.285h3.451v13.113h7.754V7.285H57.249z"/>
+								<path fill="#FFFFFF" d="M68.129,15.201c2.381,0,4.128,0.467,5.238,1.401c1.109,0.934,1.664,2.104,1.664,3.512v1.949v8.891v1.178    c0,0.487-0.088,1.021-0.264,1.604c-0.176,0.582-0.521,1.117-1.035,1.604c-0.515,0.487-1.218,0.893-2.112,1.218    c-0.893,0.325-2.057,0.487-3.491,0.487s-2.599-0.162-3.491-0.487c-0.894-0.325-1.597-0.731-2.111-1.218    c-0.515-0.487-0.859-1.021-1.036-1.604c-0.176-0.582-0.264-1.117-0.264-1.604v-1.908c0-1.028,0.264-1.963,0.792-2.802    c0.527-0.839,1.278-1.434,2.252-1.787c0.488-0.163,1.042-0.351,1.665-0.568c0.623-0.216,1.225-0.426,1.807-0.629    c0.582-0.203,1.089-0.386,1.523-0.548c0.432-0.163,0.703-0.257,0.812-0.285c0.108-0.054,0.25-0.128,0.426-0.223    c0.176-0.095,0.345-0.237,0.507-0.426c0.163-0.189,0.305-0.406,0.427-0.649c0.122-0.244,0.182-0.528,0.182-0.853v-0.771    c0-0.271-0.054-0.548-0.162-0.833c-0.108-0.284-0.298-0.548-0.568-0.791c-0.271-0.244-0.629-0.44-1.076-0.589    c-0.446-0.148-1.009-0.223-1.685-0.223c-1.354,0-2.267,0.278-2.741,0.832c-0.473,0.556-0.71,1.226-0.71,2.011h-3.451v-0.975    c0-0.676,0.128-1.312,0.386-1.908c0.257-0.595,0.669-1.117,1.238-1.563s1.285-0.798,2.152-1.056    C65.869,15.33,66.911,15.201,68.129,15.201z M71.62,25.96c-0.054,0.271-0.189,0.474-0.405,0.609    c-0.217,0.135-0.419,0.23-0.609,0.284c-0.406,0.136-0.846,0.291-1.319,0.467c-0.474,0.176-0.914,0.332-1.32,0.467    c-0.487,0.162-0.948,0.325-1.379,0.487c-0.325,0.136-0.637,0.325-0.935,0.569c-0.243,0.217-0.467,0.494-0.67,0.832    c-0.203,0.339-0.305,0.765-0.305,1.278c0,1.029,0.244,1.794,0.731,2.294c0.487,0.501,1.393,0.751,2.72,0.751    c1.354,0,2.273-0.264,2.761-0.792c0.487-0.528,0.73-1.279,0.73-2.254V25.96z"/>
+								<path fill="#FFFFFF" d="M92.854,36.72h-3.451V21.129v-0.365c0-0.244-0.034-0.508-0.102-0.792    c-0.067-0.285-0.223-0.555-0.466-0.812c-0.244-0.257-0.589-0.474-1.036-0.65c-0.446-0.176-1.036-0.264-1.766-0.264    c-0.758,0-1.374,0.088-1.847,0.264c-0.474,0.176-0.839,0.386-1.096,0.629c-0.258,0.244-0.427,0.508-0.507,0.792    c-0.082,0.284-0.122,0.535-0.122,0.751v0.446V36.72h-3.451V20.845v-0.731c0-0.325,0.054-0.757,0.163-1.299    c0.108-0.542,0.399-1.082,0.873-1.624c0.474-0.541,1.171-1.008,2.091-1.401c0.92-0.391,2.192-0.588,3.816-0.588    c1.624,0,2.896,0.197,3.817,0.588c0.92,0.393,1.61,0.86,2.07,1.401c0.46,0.542,0.745,1.09,0.853,1.644    c0.108,0.555,0.162,0.982,0.162,1.279v0.406V36.72z"/>
+							</g>
+							<path fill="#FFFFFF" d="M6.39,7.361l20.138-0.076l2.305,3.271L4.148,10.631L6.39,7.361z M14.795,27.319V9.791h3.253v17.668l0,0   c0,0,0.732,7.062,4.567,8.311l0.464,1.305l-4.895-0.032h-0.136h-3.253h-4.676l0.677-1.292   C14.679,33.541,14.795,27.319,14.795,27.319z"/>
+							<rect x="83.442" y="9.121" fill="#ADABA9" width="4.978" height="4.978"/>
+							<rect fill="none" width="250.899" height="40.297"/>
+						</g>
+						</svg>
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+			 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav ml-auto">
+					<!--購物車-->
+
+					<!---->
+
+					<li class="nav-item active">
+						<a class="nav-link hvr-icon-pulse" style="width:130px;font-size: 17px;text-align:center " href="shopping.php"><i class="fas fa-home hvr-icon"></i>&nbsp;線上商城
+							<span class="sr-only">(current)</span></a>
+					</li>
+
+					<li class="nav-item">
+						<a class="nav-link hvr-icon-pulse" style="width:130px; font-size: 17px;text-align:center " href="http://casa.tahan.com.tw/"><i class="fas fa-bug hvr-icon"></i>&nbsp;專業客製</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link hvr-icon-pulse" style="width:130px; font-size: 17px;text-align:center " href="https://www.tahan.com.tw/about"><i class="fas fa-medal hvr-icon"></i>&nbsp;公司簡介</a>
+					</li>
+					
+					<li class="nav-item">
+						<!-- data-toggle="modal" data-target="#exampleModal" -->
+						<a class="nav-link hvr-icon-pulse" style="width:130px; font-size: 17px;text-align:center" id="chklogin" href="javascript:void(0)
+						">
+							<i class="far fa-user hvr-icon"></i>&nbsp;會員登入
+						</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link hvr-icon-pulse" style="width:70px;font-size: 16px; " href="cart.php">
+							<div class="buycart">
+								<i class="fas fa-cart-plus hvr-icon"></i>
+								<?php 
+									if(!empty($_SESSION['cart'])){
+										echo count($_SESSION['cart']);
+									}
+									?>
+							</div>
+						</a>
+					</li>
+
+
+
+
+				</ul>
+
+				<ul class="navbar-nav m-auto d-block d-lg-none">
+					<!--d-block d-md-none-->
+					<li class="nav-item" style="font-size:16px">
+						<a class="nav-link text-center float-left active" href="#d1" style="width:14vw">生活提案 <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item" style="font-size:16px">
+						<a class="nav-link text-center" href="#d2" style="width:14vw">人氣排名</a>
+					</li>
+					<li class="nav-item" style="font-size:16px">
+						<a class="nav-link text-center" href="#d3" style="width:14vw">商標故事</a>
+					</li>
+
+					<li class="nav-item" style="font-size:16px">
+						<a class="nav-link text-center" href="#d4" style="width:14vw">特別內容</a>
+					</li>
+					<li class="nav-item" style="font-size:16px">
+						<a class="nav-link text-center" href="#d5" style="width:14vw">優惠專案</a>
+					</li>
+
+					<li class="nav-item" style="font-size:16px">
+						<a class="nav-link text-center" href="#d6" style="width:14vw">最新消息</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+</div>
+</div>
+<!--增加購物車JS-->
+<script>
+	$(".buycart").hover(
+		function () {
+			$.get("api.php?do=getCartList", function (res) {
+				let data = JSON.parse(res)
+				let str = "";
+				$.each(data, function (index, value) {
+					str += "<li>" + value.name + " / " + value.price + " / " + value.qt + "</li>"
+				})
+
+				$(".cart-repo").html(str)
+			})
+			$(".cart-repo").show()
+		},
+		function () {
+			$(".cart-repo").hide()
+		}
+	)
+	$(".cart-repo").hover(function () {
+			$(this).show()
+		},
+		function () {
+			$(this).hide()
+		})
+</script>
+
+<script>
+	/*buycart購物車*/
+	$(".item-cart-btn").click(function () {
+		let id = $(this).attr("id").split("-")[1];
+		let qt = 1;
+		//console.log(id,num)
+		//location.href="cart.php?id="+id+"&qt="+num
+		$.get("cart.php", {
+			id,
+			qt
+		}, function () {
+			let carttext = $(".buycart").text().trim();
+			console.log(carttext)
+			let now = 0;
+			if (carttext != "") {
+				now = parseInt(carttext);
+			}
+			$(".buycart").text(now + 1);
+		})
+
+	})
+	//登出功能
+	function logout() {
+		$.post("./ajax_login/api.php?do=logout", function () {
+			$("#chklogin").html(`<a class="nav-link p-0 hvr-icon-pulse"" href="./ajax_login/sign.html"><i class="far fa-user hvr-icon">會員登入</a>`)
+		})
+	}
+
+
+
+	//載入登入畫面
+	function lo(page) {
+		$("#login").load(page, function () {
+			if (page == './ajax_login/sign.html') {
+				$.get("./ajax_login/api.php?do=makeCode", function (code) {
+					$("#login img").attr("src", "data:image/png;base64," + code)
+				})
+			}
+			$("#login").show();
+			$("#main").hide();
+			$("#intro").hide();
+		})
+	}
+
+	//檢查是否為登入狀態
+	$.get("./ajax_login/api.php?do=chkLogin", function (chk) {
+		if (chk > 0) {
+			$("#chklogin").html(
+				`<a class="nav-link p-0 hvr-icon-pulse" href="javascript:logout()" style="color:red;border:1px solid #F00 ">&nbsp;<i class="far fa-user hvr-icon"></i>&nbsp;登出&nbsp;</a>`
+			)
+		} else {
+			$("#chklogin").html(`<a class="nav-link hvr-icon-pulse p-0" href="./ajax_login/sign.html"><i class="far fa-user hvr-icon"></i>會員登入</a>`)
+		}
+	})
+</script>
+<!---->
+<div id="loading" class="w-100 h-100 position-fixed fixed-top bg-white">
+	<table class="w-100 h-100 text-center">
+		<tr>
+			<td class="align-middle">
+<svg width="10%" height="10%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid"
+ class="lds-bricks">
+	<rect ng-attr-fill="{{config.c1}}" ng-attr-x="{{config.x}}" ng-attr-y="{{config.x}}" ng-attr-width="{{config.w}}"
+	 ng-attr-height="{{config.w}}" ng-attr-rx="{{config.radius}}" ng-attr-ry="{{config.radius}}" fill="#030303" x="55" y="15"
+	 width="30" height="30" rx="3" ry="3">
+		<animate attributeName="x" calcMode="linear" values="15;55;55;55;55;15;15;15;15" keyTimes="0;0.083;0.25;0.333;0.5;0.583;0.75;0.833;1"
+		 dur="2" begin="-1.8333333333333333s" repeatCount="indefinite"></animate>
+		<animate attributeName="y" calcMode="linear" values="15;55;55;55;55;15;15;15;15" keyTimes="0;0.083;0.25;0.333;0.5;0.583;0.75;0.833;1"
+		 dur="2" begin="-1.3333333333333333s" repeatCount="indefinite"></animate>
+	</rect>
+	<rect ng-attr-fill="{{config.c2}}" ng-attr-x="{{config.x}}" ng-attr-y="{{config.x}}" ng-attr-width="{{config.w}}"
+	 ng-attr-height="{{config.w}}" ng-attr-rx="{{config.radius}}" ng-attr-ry="{{config.radius}}" fill="#d39182" x="15" y="31.2355"
+	 width="30" height="30" rx="3" ry="3">
+		<animate attributeName="x" calcMode="linear" values="15;55;55;55;55;15;15;15;15" keyTimes="0;0.083;0.25;0.333;0.5;0.583;0.75;0.833;1"
+		 dur="2" begin="-1.1666666666666667s" repeatCount="indefinite"></animate>
+		<animate attributeName="y" calcMode="linear" values="15;55;55;55;55;15;15;15;15" keyTimes="0;0.083;0.25;0.333;0.5;0.583;0.75;0.833;1"
+		 dur="2" begin="-0.6666666666666666s" repeatCount="indefinite"></animate>
+	</rect>
+	<rect ng-attr-fill="{{config.c3}}" ng-attr-x="{{config.x}}" ng-attr-y="{{config.x}}" ng-attr-width="{{config.w}}"
+	 ng-attr-height="{{config.w}}" ng-attr-rx="{{config.radius}}" ng-attr-ry="{{config.radius}}" fill="#e0e0d0" x="55" y="55"
+	 width="30" height="30" rx="3" ry="3">
+		<animate attributeName="x" calcMode="linear" values="15;55;55;55;55;15;15;15;15" keyTimes="0;0.083;0.25;0.333;0.5;0.583;0.75;0.833;1"
+		 dur="2" begin="-0.5s" repeatCount="indefinite"></animate>
+		<animate attributeName="y" calcMode="linear" values="15;55;55;55;55;15;15;15;15" keyTimes="0;0.083;0.25;0.333;0.5;0.583;0.75;0.833;1"
+		 dur="2" begin="0s" repeatCount="indefinite"></animate>
+	</rect>
+</svg>
+			</td>
+		</tr>
+	</table>
+</div>
+<script>
+	$(document).ready(function(){
+		setTimeout("$('#loading').fadeOut(1000)",1000);
+	})
+</script>
+<!---->
